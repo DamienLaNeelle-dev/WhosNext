ebdaf6a2611a3f183c291e14f8a35ce3d983eb10
Damien L <damien.laneelle33@gmail.com>
2025-03-25 18:45:35 +0100

    wip

--- app/src/main/java/com/example/whosnext/HomeScreen..kt
+++ app/src/main/java/com/example/whosnext/HomeScreen..kt
@@ -3,18 +3,31 @@ package com.example.whosnext
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
+
+    val navController = rememberNavController()
+
     Scaffold(
         topBar = {
+            NavHost(navController = navController, startDestination = "HomeScreen"){
+                composable("HomeScreen") { navController }
+                composable("login") { MainActivity() }
+            }
+            LoginButton(navController = navController)
             TopAppBar(
                 title = { Text("Home Screen") }
             )
@@ -30,3 +43,14 @@ fun HomeScreen() {
         }
     )
 }
+
+@Composable
+fun LoginButton(navController: NavController){
+    Button(
+        onClick = {navController.navigate("login")}
+    ){
+        Text(text = "X")
+    }
+}
+
+
